5b19cf9620da59805a98be5bbcc869f69a80d686
KadenMK394 <kadenmk394@gmail.com>
2024-12-17 14:01:51 -0600

    added DTOs for reimbursements

---
+++ P1 Backend/src/main/java/com/revature/models/DTOs/IncomingReimbDTO.java
@@ -0,0 +1,61 @@
+package com.revature.models.DTOs;
+
+public class IncomingReimbDTO {
+    private String description;
+    private int amount;
+    private String status;
+    private int userId;
+
+    //boilerplate-----------------------
+    public IncomingReimbDTO() {
+    }
+
+    public IncomingReimbDTO(String description, int amount, String status, int userId) {
+        this.description = description;
+        this.amount = amount;
+        this.status = status;
+        this.userId = userId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public String toString() {
+        return "IncomingReimbDTO{" +
+                "description='" + description + '\'' +
+                ", amount=" + amount +
+                ", status='" + status + '\'' +
+                ", userId=" + userId +
+                '}';
+    }
+}
--- P1 Backend/src/main/java/com/revature/services/UserService.java
+++ P1 Backend/src/main/java/com/revature/services/UserService.java
@@ -2,10 +2,13 @@ package com.revature.services;
 
 import com.revature.DAOs.ReimbDAO;
 import com.revature.DAOs.UserDAO;
+import com.revature.models.DTOs.IncomingUserDTO;
+import com.revature.models.DTOs.OutgoingUserDTO;
 import com.revature.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -20,20 +23,32 @@ public class UserService {
     }
 
     //Logged out - Insert a new User (create a new account)
-    public User insertUser(User user){
-        /*Throw errors if:
+    public User insertUser(IncomingUserDTO userDTO){
+        //TODO: make sure the userDTO fields are present and valid
+        //TODO: make sure the incoming username is unique
+        /*TODO: Throw errors if:
         * First name is blank or null
         * Last name is blank or null
         * Username is blank or null
         * Password is blank or null
         * Username already exists
         * */
+        User user = new User(0, userDTO.getFirstName(), userDTO.getLastName(), userDTO.getUsername(), userDTO.getPassword(), userDTO.getRole(), null);
+
+        //TODO: apply an empty reimbursement list or populate reimbursements (?)
+
         return userDAO.save(user);
     }
     //Manager - get a list of all Users
-    public List<User> getAllUsers(){
-        //Throw an error if there are no records (shouldn't ever happen)
-        return userDAO.findAll();
+    public List<OutgoingUserDTO> getAllUsers(){
+        //TODO: Throw an error if there are no records (shouldn't ever happen)
+        List<OutgoingUserDTO> outgoingUsers = new ArrayList<OutgoingUserDTO>();
+        List<User> users = userDAO.findAll();
+        for(User user: users){
+            //TODO: remember to add reimbursements list to outgoing users
+            outgoingUsers.add(new OutgoingUserDTO(user.getUserId(), user.getFirstName(), user.getLastName(), user.getUserName(), user.getRole()));
+        }
+        return outgoingUsers;
     }
     //Manager - delete a User (and delete their Reimbursements)
     public User deleteUser(int userId){
